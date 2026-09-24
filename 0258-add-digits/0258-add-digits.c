b22c731203e90f7744d90123fdae0f5ec54e5f2f
SaiKarthiksontela <ksai27117@gmail.com>
2026-09-24 07:02:50 +0530

    Time: 0 ms (100%), Space: 9.1 MB (39.43%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.c
@@ -0,0 +1,15 @@
+int addDigits(int num) {
+    
+    while(num >= 10){
+        int sum = 0;
+    while(num>0){
+        int last = num%10;
+        sum += last;
+        num /= 10;    
+    }
+        
+            num = sum;
+    }
+
+    return num;
+}
